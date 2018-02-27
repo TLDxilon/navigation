0ef005fe115b426f2aa7a5a4e3a43e7630293575
toya <toya.dapena@gmail.com>
2018-02-27 11:38:24 +0100

    dev

--- js/nav.js
+++ js/nav.js
@@ -1,36 +1,41 @@
 $(document).ready(function(){
-
-var $navBarFixed = $('.navbar-fixed');
-
-
-$(window).on('scroll', function(){
-    // Hago que baje al darle al scroll hacia arriba
-    var newScrollPosition = window.scrollY;
-
-    // Detectamos que el scroll sube
-    if (newScrollPosition < lastScrollPosition){
-
-        console.log('newScrollPosition', newScrollPosition);
-        console.log('lastScrollPosition',lastScrollPosition);
+    var lastScrollPosition = 0;
+    var $navBarFixed = $('.navbar-fixed');
 
 
-        // Oculta la navegación cuando está a la altura de la cabecera
-        if (newScrollPosition < 400){
-            $navBarFixed.addClass('is-hide');
-        } else {
-            // Muestro la navegación al subir
-            $navBarFixed.removeClass('is-hide');
+
+
+    $(window).on('scroll', function(){
+        // Hago que baje al darle al scroll hacia arriba
+
+        var newScrollPosition = window.scrollY;
+
+        // Muestro la navegación al subir
+        if (newScrollPosition < lastScrollPosition){
+
+            console.log('newScrollPosition',newScrollPosition);
+            console.log('lastScrollPosition',lastScrollPosition);
+
+
+            // Oculta la navegación cuando está a la altura de la cabecera
+            if (newScrollPosition < 500){
+                $navBarFixed.addClass('is-hide');
+
+            } else {
+                $navBarFixed.removeClass('is-hide');
+
+            }
+
         }
 
-    }
-
-    // Oculto la navegación al bajar
-    if (newScrollPosition > lastScrollPosition){
-        $navBarFixed.addClass('is-hide');
-    }
+        // Oculto la navegación al bajar
+        if (newScrollPosition > lastScrollPosition){
+            $navBarFixed.addClass('is-hide');
+        }
 
         lastScrollPosition = newScrollPosition;
-
-
     });
+
+
+
 });
