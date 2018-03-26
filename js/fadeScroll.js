b8778d52eeb19029653a91ee68e69582b2b2f2bf
toya <toya.dapena@gmail.com>
2018-03-26 12:48:17 +0200

    dev

---
+++ js/fadeScroll.js
@@ -0,0 +1,16 @@
+var fadeStart=100
+    ,fadeUntil=300
+    ,fading = $('.js-fixed.bg-transparent')
+;
+
+$(window).bind('scroll', function(){
+    var offset = $(document).scrollTop()
+        ,opacity=0
+    ;
+    if( offset<=fadeStart ){
+        opacity=1;
+    }else if( offset<=fadeUntil ){
+        opacity=1-offset/fadeUntil;
+    }
+    fading.css('opacity',opacity);
+});
