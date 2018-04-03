b015e53a9498668b3c9d4f43e0418235fdd569ef
toya <toya.dapena@gmail.com>
2018-04-03 09:13:50 +0200

    dev

--- _site/js/disappearOnScroll.js
+++ _site/js/disappearOnScroll.js
@@ -1,20 +1,22 @@
-var dissapear = $('.dissapear');
-var offsetDissapear = dissapear.offset().top;
-console.log("OFFSET DISSAPEAR", offsetDissapear);
-var fadeStart = 100;
-var fadeUntil = offsetDissapear + 400;
 
 
 
+if ($('.dissapear').length) {
+    $(window).bind('scroll', function () {
+        var dissapear = $('.dissapear');
+        var offsetDissapear = dissapear.offset().top;
+        console.log("OFFSET DISSAPEAR", offsetDissapear);
+        var fadeStart = 100;
+        var fadeUntil = offsetDissapear + 400;
 
-$(window).bind('scroll', function(){
-    var offset = $(document).scrollTop()
-        ,opacity=0
-    ;
-    if( offset<=fadeStart ){
-        opacity=1;
-    }else if( offset<=fadeUntil ){
-        opacity=1-offset/fadeUntil;
-    }
-    dissapear.css('opacity',opacity);
-});
+        var offset = $(document).scrollTop()
+            , opacity = 0
+        ;
+        if (offset <= fadeStart) {
+            opacity = 1;
+        } else if (offset <= fadeUntil) {
+            opacity = 1 - offset / fadeUntil;
+        }
+        dissapear.css('opacity', opacity);
+    });
+}
